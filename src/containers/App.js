e091c779155501db2bbae510bd7ab6d739f9c573
Ayoola David <ayooladavid@gmail.com>
2020-06-15 16:31:28 +0100

    Add edited files

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import styled, { createGlobalStyle } from 'styled-components';
 import Board from './Board';
+import Tickets from './Tickets'
 import Header from '../components/Header/Header';
+
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -31,10 +33,11 @@ class App extends Component {
 
    return (
       <>
-        <GlobalStyle />
+        <GlobalStyle/>
         <AppWrapper>
           <Header />
           <Board lanes={lanes} dataSource={'../../assets/data.json'} />
+          <Tickets dataSource={'../../assets/data.json'} />
         </AppWrapper>
       </>
     );
